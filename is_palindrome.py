2756cb0b8457b533c88f7d10dda5a3dd51e6c885
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 15:01:53 -0700

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -23,3 +23,27 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 
 """
+
+def isPalindrome(s):
+
+    if s == "":    # an empty string is a palindrome
+        return True
+
+    start = 0
+    end = len(s) - 1
+
+    while(start <= end):
+
+        while s[start].isalpha() == False and s[start].isdigit() == False and start < end:
+            start += 1
+
+        while s[end].isalpha() == False and s[end].isdigit() == False and start < end:
+            end -= 1
+
+        if s[start].lower() != s[end].lower():
+            return False
+
+        start += 1
+        end -= 1
+
+    return True
