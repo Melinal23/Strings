6eab174b2840e47b2507c5684f23239ea1faab86
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 14:38:10 -0700

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,19 +1,19 @@
-    def isPalindrome(self, s: str) -> bool:
-         
-        if s == "":    # an empty string is a palindrome
-            return True
-        
-        s = "".join([x for x in s if x.isalpha() or x.isdigit()])
-        
-        start = 0
-        end = len(s) - 1
-        
-        while(start <= end):
-            
-            if s[start].lower() != s[end].lower():
-                return False
-            
-            start += 1
-            end -= 1
-        
+def isPalindrome(self, s: str) -> bool:
+
+    if s == "":    # an empty string is a palindrome
         return True
+
+    s = "".join([x for x in s if x.isalpha() or x.isdigit()])
+
+    start = 0
+    end = len(s) - 1
+
+    while(start <= end):
+
+        if s[start].lower() != s[end].lower():
+            return False
+
+        start += 1
+        end -= 1
+
+    return True
