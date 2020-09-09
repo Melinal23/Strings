99e2d2e9e621484586ba01196946871634a02c32
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 15:13:28 -0700

    Update str_str.py

--- str_str.py
+++ str_str.py
@@ -2,6 +2,9 @@ def strStr(haystack, needle):
 
     if len(needle) == 0:
         return 0
+    
+    if len(haystack) = 0:
+        return -1
 
     if haystack.find(needle) != -1:
         return haystack.index(needle)
