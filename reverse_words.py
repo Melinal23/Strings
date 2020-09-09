bef6cce32b031eccfe8e34ded2467129cfa775f7
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 15:43:20 -0700

    Update reverse_words.py

--- reverse_words.py
+++ reverse_words.py
@@ -12,5 +12,28 @@ print(reverseWords("Let's take LeetCode contest"))
 
 
 #the same as above, just in one line
-def reverseWords_student_soln(s):
+def reverseWords(s):
     return " ".join([word[::-1] for word in s.split()])
+
+
+def reverseWords(s):
+    if len(s) == 0:
+        return s
+
+    s = s.split()
+
+    start = 0
+    end = len(s) - 1
+
+    while(start <= end):
+
+        s[start], s[end] = s[end], s[start]
+
+        start += 1
+        end -= 1
+
+    return " ".join(s)
+
+"""
+Time Complexity: O(n)
+"""
