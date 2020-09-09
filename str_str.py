806318f27fd3caac5f43167e6727e733d07b287e
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-09 15:31:17 -0700

    Update str_str.py

--- str_str.py
+++ str_str.py
@@ -10,3 +10,24 @@ def strStr(haystack, needle):
         return haystack.index(needle)
     else:
         return -1
+
+ # without built-in functions
+def strStr(haystack, needle):
+
+    if len(needle) == 0:
+        return 0
+
+    if len(haystack) == 0 or len(needle) > len(haystack):
+        return -1
+
+    for i in range(len(haystack)):
+        for j in range(len(needle)):
+            if i + j == len(haystack):
+                return -1
+            if needle[j] != haystack[i+j]:
+                break 
+            if j == len(needle)-1: 
+                return i
+
+    return -1
+        
