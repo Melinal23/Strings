9605f1a5bd33d012e0a19dbf9f04808714d0e880
Melina Lopez <38011974+Melinal23@users.noreply.github.com>
2020-09-11 16:41:51 -0700

    Create reverse_vowels.py

---
+++ reverse_vowels.py
@@ -0,0 +1,39 @@
+"""
+Given a string as an input, return a copy of that string, except with the order of all of its
+vowels reversed. The order of the constants should stay the same.
+
+For example, given the string word = "san francisco", the output would be "son frincasca."
+
+Return a copy of the original string with its vowels reversed.
+"""
+def reverse_vowels(word):
+
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    temp = []
+
+    for char in word:
+        temp.append(char)
+
+    start = 0
+
+    end = len(temp) - 1
+
+    while(start <= end):
+
+        while start <= end and temp[start] not in vowels:
+            start += 1
+
+        while end >= start and temp[end] not in vowels:
+            end -= 1
+
+
+        print("start", start)
+        print("end", end)
+
+
+        if start <= end and end >= start:
+            temp[start], temp[end] = temp[end], temp[start]
+            start += 1
+            end -= 1
+
+    return "".join(temp)
